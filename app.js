0f716d51d540137ff077f6bc63f29d6cfcf672db
YaseenFsharid <yaseen.sharid@gmail.com>
2020-02-20 12:43:49 +0200

    solve the third fun

--- app.js
+++ app.js
@@ -54,11 +54,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+var sum1= sum(a, b);
+var sum2= sum(sum1[0],c);
+var finalsum= sum2[0];
 
+var mul1= multiply(a,b);
+var mul2= multiply(mul1[0],c);
+var fmul= mul2[0];
+
+var msg1 = `${a} and ${b} and ${c} sum to ${finalsum}.`;
+var msg2 = `The product of ${a} and ${b} and ${c} is ${fmul}.`;
+return [finalsum ,fmul,msg1,msg2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
